97ae52b9090cbdc4c50bcdb62335614a469130db
RishabMangal <rishabhmangal1@gmail.com>
2021-09-30 18:29:53 +0530

    cat filte

--- src/components/Bills.js
+++ src/components/Bills.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import Bill from "./Bill";
 const Bills = (props) => {
+  const [category, setCat] = useState("");
+  const [billsList, setBills] = useState([...props.billsList]);
+  const filterBills = () => {
+    if (category) {
+      let temp = props.billsList.filter((bill) => bill.category === category);
+      setBills([...temp]);
+    } else setBills([...props.billsList]);
+  };
   return (
     <div>
+      <div className="m-4">
+        <div>Filter By : </div>
+        <div>
+          <input
+            value={category}
+            onChange={(e) => setCat(e.target.value)}
+            placeholder={"Category"}
+          ></input>
+          <button className="btn btn-primary" onClick={filterBills}>
+            Filter
+          </button>
+        </div>
+      </div>
       <table className="table">
         <tbody>
-          {props.billsList?.map((bill, i) => (
+          {billsList?.map((bill, i) => (
             <tr key={bill.id}>
               <Bill {...bill} {...props}></Bill>
             </tr>
